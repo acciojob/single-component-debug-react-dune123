29dfb5505f9ee2dcdba0f52b5c09426d3053c689
sraj39273@gmail.com <sraj39273@gmail.com>
2024-12-18 12:23:22 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// index.js
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client"; // Make sure to import from 'react-dom/client'
 import App from "./components/App";
 
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
